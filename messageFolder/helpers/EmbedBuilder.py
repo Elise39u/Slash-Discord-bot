2d0b00a28c6a28c04e9948a7fa9167258b3003da
Elise <justin555@live.nl>
2023-04-07 10:37:25 +0000

    Rewrite /help function and added a embed builder with author url

--- messageFolder/helpers/EmbedBuilder.py
+++ messageFolder/helpers/EmbedBuilder.py
@@ -17,4 +17,22 @@ async def BuildEmbed(message, embedTitle, embedDescription, imageUrl, colour,
       await channel.send(embed=newEmbed)
     else: 
       await message.response.send_message(embed=newEmbed)
-                    
+
+
+async def EmbedWithAuthorBuilder(message, embedTitle, embedDescription, imageUrl, colour,
+                     footer, channel, url, authorName, authorImage):
+    newEmbed = discord.Embed(title=embedTitle, description=embedDescription)
+    newEmbed.set_author(name=authorName, icon_url=authorImage)
+    if not colour:
+        newEmbed.colour = 0xfe3ee4
+    else:
+        newEmbed.colour = colour
+    if len(imageUrl) > 0:
+        newEmbed.set_image(url=imageUrl)
+    newEmbed.set_footer(text=footer)
+    if url:
+      newEmbed.url = url;
+    if message is None:
+      await channel.send(embed=newEmbed)
+    else: 
+      await message.response.send_message(embed=newEmbed)
